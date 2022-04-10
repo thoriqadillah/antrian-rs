f8b2b1544096f81fe59619e2005a74e31bd5dd43
Muhammad Faisal <muhammadfaisaltr3@gmail.com>
2022-04-10 21:41:40 +0700

    list antrian box

--- resources/views/antrian/antrian.blade.php
+++ resources/views/antrian/antrian.blade.php
@@ -11,18 +11,22 @@
     <h1>List Antrian Rumah Sakit</h1>
     <div class="container">
         <a class="mb-2" href="{{route('antrian.pendaftaran')}}">
-            <button type="button" class="btn btn-success" >Daftar Antrian</button>
+            <button type="button" class="btn-daftar" style="margin-bottom:20px;" >Daftar Antrian</button>
         </a>
     </div>    
     <div class="container" id="antrian"></div>
 
     @auth
-        <div class="container card-group">
-            <div class="card border-dark text-center my-5">
-                <div class="card-header">Rumah Sakit Terserah</div>
-                <div class="card-body">
-                    <h5 class="card-title">Nomor Antrian Anda</h5>
-                    <h1 class="card-text">{{ $loket . '' . $nomor }}</h1>
+        <div class="container card-group" id="antrian-wrapper">
+            <div class="card border-dark text-center my-5" id="lists">
+                <div class="antrian">
+                    <div class="card-headers">
+                        <img src="https://i.ibb.co/K2L3hnF/stethoscope-icon-gfe1c62d5a-1280.png" class="img">
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nomor Antrian Anda</h5>
+                        <h1 class="card-text">{{ $loket . '' . $nomor }}</h1>
+                    </div>
                 </div>
             </div>
         </div>
@@ -50,14 +54,18 @@
                 let arr = ['A', 'B', 'C'];
                 for(let i = 0;i < 3; i++){
                     $('#antrian').append($(
-                        `<div class='card-group'>
-                            <div class='card'>
-                                <div class="card-body">
-                                    <h3 class='card-title text-center'>${arr[i]}${polis[i]['nama_poli']}</h3>
-                                    <h1>${arr[i]}${nomors[i]['nomor']}</h1>
-                                </div>
+                        `<div class="antrian-wrapper">
+                            <div class="container">
+                                <div class="card-headers">
+                                    <img src="https://i.ibb.co/K2L3hnF/stethoscope-icon-gfe1c62d5a-1280.png" class="img">
+                                        <div class="card-body">
+                                            <h3 class='card-title text-center'>${arr[i]} ${polis[i]['nama_poli']}</h3>
+                                            <h1>${arr[i]}${nomors[i]['nomor']}</h1>
+                                        </div>
+                                 </div>
                             </div>
-                        </div>`
+                        </div>
+                        `
                         )
                     )
                     }
